15048f7bb77c81110aca5fb74e552c72bb3a8c60
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-09-08 11:26:25 -0600

    Initial commit. Added projects.

---
+++ XUnitIntroPractice/XUnitIntroPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XUnitIntroPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
