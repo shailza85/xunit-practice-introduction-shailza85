aa90e0ce4fd3e9cfa3972c8690a974f06c23b90c
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-09-08 21:17:34 -0600

    Added XUnit Test and methods.

--- XUnitIntroPractice/XUnitIntroPractice/Program.cs
+++ XUnitIntroPractice/XUnitIntroPractice/Program.cs
@@ -1,12 +1,91 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace XUnitIntroPractice
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HelloWorld();
+        }
+        public static void HelloWorld()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+
+
+        public static bool IsYesOrNo(string myString)
+        {
+
+            if (myString == "Yes".Trim().ToLower() || myString == "Y".Trim().ToLower() || myString == "No".Trim().ToLower() || myString == "N".Trim().ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //Write a method called IsYesOrNo that will accept a string and return true if the string is “YES”, “Y”, “NO”, or “N”, in any casing, trimmed. Otherwise it will return false.
+
+        public static bool Str_ToUpper(string upStr)
+        {
+            if (upStr == "yes".ToUpper() || upStr == "y".ToUpper() || upStr == "no".ToUpper())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool AcceptValue(string inputValue)
+        {
+            string strVal = inputValue;
+            if (strVal.ToLower() == inputValue)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool InvalidValue(string invalidVal)
+        {
+            string strVal1 = invalidVal;
+            if (invalidVal == "" || invalidVal.Length == 1 || invalidVal.Length > 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static void NullExec()
+        {
+            int[] values = null;
+            for (int ctr = 0; ctr <= 9; ctr++)
+            {
+                values[ctr] = ctr * 2;
+            }
+            foreach (var value in values)
+                Console.WriteLine(value);
+        }
+
+        //@link: https://docs.microsoft.com/en-us/dotnet/api/system.indexoutofrangeexception?view=netcore-3.1
+        public static int[] PopulateArray(int items, int maxValue)
+        {
+            int[] values = new int[items];
+            Random rnd = new Random();
+            for (int ctr = 0; ctr < items; ctr++)
+                values[ctr] = rnd.Next(0, maxValue + 1);
+ foreach (var value in values)
+                Console.Write("{0}   ", values[value]);
+            return values;
+           
         }
     }
 }
--- XUnitIntroPractice/XUnitIntroPractice_Tests/UnitTest1.cs
+++ XUnitIntroPractice/XUnitIntroPractice_Tests/UnitTest1.cs
@@ -1,14 +1,114 @@
 using System;
 using Xunit;
+using XUnitIntroPractice;
 
 namespace XUnitIntroPractice_Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public static void Test1()
         {
 
+            Program.HelloWorld();
+        }
+        //Write XUnit tests that will test the trimming of whitespace (both spaces and tabs) before and after the text.
+        //9 test case permutations.
+
+        [Theory,
+         InlineData("Yes", true),
+         InlineData("Y", true),
+         InlineData("NO", true),
+         InlineData(" N", true),
+         InlineData(" Yes", true),
+         InlineData(" Y", true),
+         InlineData(" NO ", true),
+         InlineData(" N   ", true),
+         InlineData("    N ", true)]
+
+
+
+        public void Contains_Test(string myString1, bool result)
+        {
+         
+            Assert.True(result, myString1);
+
+        }
+
+        //Write XUnit tests that will test the conversion to uppercase.
+        //3 test case permutations.
+
+        [Theory,
+             InlineData("yes",true),
+             InlineData("y", true),
+             InlineData("no", true)]
+
+        public void Str_ToUpper(string newStr, bool upperStr)
+        {
+            Assert.True(upperStr, newStr);
+        }
+
+        //Write XUnit tests for all accepted values.
+        //4 test case permutations.
+
+        [Theory,
+             InlineData("hi", true),
+             InlineData("HELLO1", true),
+            InlineData("MaRK", true),
+             InlineData("John", true)
+             ]
+
+        public void AcceptValue(string newStr1, bool anyStr)
+        {
+            Assert.True(anyStr, newStr1);
+        }
+
+        //Write XUnit tests for invalid values. Include at least one single character invalid value, one multiple character one, and an empty string.
+
+        [Theory,
+             InlineData("c", false),
+             InlineData("", false),
+            InlineData("no", false),
+            
+             ]
+
+        public void InvalidValue(string newStr2, bool invalidStr)
+        {
+            Assert.False(invalidStr, newStr2);
+        }
+
+
+        [Theory,
+       InlineData("yes")
+       ]
+        public void IntParse_Test_Invalid(string test)
+        {
+            // Throws will require that a specific exception type is thrown, not any derived type, or any other type.
+            Assert.Throws<FormatException>(() => int.Parse(test));
+           
+        }
+
+        //Write assertions using Throws<FormatException>, Throws<IndexOutOfRangeException>, Throws<NullReferenceException> and Throws<DivideByZero Exception>,
+        [Theory,
+        InlineData(10, 0),
+        InlineData(5, 0)]
+        public void Divide(int one, int two)
+        {
+            Assert.Throws<DivideByZeroException>(() => Program.Divide(one, two));
+        }
+
+        [Theory,
+        InlineData()]
+        public void NullExec()
+        {
+            Assert.Throws<NullReferenceException>(() => Program.NullExec());
+        }
+
+        [Theory,       
+        InlineData(5, 10)]
+        public void PopulateArray(int x, int y)
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => Program.PopulateArray(x,y));
         }
     }
 }
